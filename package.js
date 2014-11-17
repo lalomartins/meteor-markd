2f02f938a9f835d5e88834adbbc161cccefeed62
Lalo Martins <lalo.martins@gmail.com>
2014-11-17 04:44:20 +0100

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'lalomartins:markd',
   summary: 'Creates js templates of your markdown files.',
-  version: '0.1.0',
+  version: '0.1.1',
   git: 'https://github.com/lalomartins/meteor-markd.git'
 });
 
